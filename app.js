187e14702774bf2f8ebf828d1ea5788b834b30ca
zhengyuhang.0926 <zhengyuhang.0926@bytedance.com>
2019-03-16 21:13:52 +0800

    feat: construct project
    Change-Id: I8fc67cdcf6796b22b3aa28c1b1814225f9a0a61e

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dataClientRouter = require('./routes/dataclient');
+var modelClientRouter = require('./routes/modelclient');
+var computingClientRouter = require('./routes/computing');
 
 var app = express();
 
@@ -21,6 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dataclient', dataClientRouter);
+app.use('/modelclient', modelClientRouter);
+app.use('computingclient', computingClientRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
