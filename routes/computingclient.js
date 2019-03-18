be00d82ff62cc6e1949180c54ae8b1c45d0daad7
AlphaPav <3160102602@zju.edu.cn>
2019-03-18 16:57:02 +0800

    feat: computing; model
    Change-Id: I519129771ea045fbc4ea16427fb5ed00212e0a36

--- routes/computingclient.js
+++ routes/computingclient.js
@@ -10,8 +10,139 @@ function completeRes(msg, code){
 }
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Computing client' });
+
+router.get('/index', function(req, res, next) {
+    res.sendFile( __dirname + "/pages/" + "indexcomputer.html" );
+});
+router.get('/walletpage', function(req, res, next) {
+    res.sendFile( __dirname + "/pages/" + "computingclientwallet.html" );
+});
+router.get('/trainpage', function(req, res, next) {
+    res.sendFile( __dirname + "/pages/" + "computing_train.html" );
+});
+
+router.get('/dataaskingpage', function(req, res, next) {
+    res.sendFile( __dirname + "/pages/" + "computing_data_ask.html" );
+});
+
+router.get('/modelaskingpage', function(req, res, next) {
+    res.sendFile( __dirname + "/pages/" + "computing_model_ask.html" );
+});
+
+
+
+router.post('/uploadfile', function (req, res) {
+    var response;
+    //todo
+    res.end(response);
+});
+
+router.post('/adddata', function (req, res) {
+    var response;
+    var password = req.body.password;
+    var from = req.body.from;
+    var computingIpfsHash = req.body.computingIpfsHash;
+
+    if(password === undefined || password === ''||
+        computingIpfsHash===undefined|| computingIpfsHash===''||
+        from ===undefined|| from ==='') {
+        response = completeRes("参数不完全", 201);
+    }
+    else {
+        //todo 运算方上传运算资源到区块链
+
+    }
+    res.end(response);
+});
+
+
+router.post('/agreerequest', function (req, res) {
+    var response;
+    var password = req.body.password;
+    var from = req.body.from;
+    var dataAddress = req.body.dataAddress;
+    var modelAddress = req.body.modelAddress;
+
+
+    if(password === undefined || password === ''||
+        dataAddress===undefined|| dataAddress===''||
+        modelAddress===undefined|| modelAddress===''||
+        from ===undefined|| from ==='') {
+        response = completeRes("参数不完全", 201);
+    }
+    else {
+        //todo 运算方的同意交易生成
+
+    }
+    res.end(response);
+});
+
+router.post('/deletedcomputing', function (req, res) {
+    var response;
+    var password = req.body.password;
+    var from = req.body.from;
+    var computinghash = req.body.computinghash;
+
+    if(password === undefined || password === ''||
+        computinghash===undefined|| computinghash===''||
+        from ===undefined|| from ==='') {
+        response = completeRes("参数不完全", 201);
+    }
+    else {
+        //todo 运算方的同意交易生成
+
+    }
+    res.end(response);
+});
+
+router.post('/trainrequest', function (req, res) {
+    var response;
+    //todo
+    res.end(response);
+});
+
+router.post('/train', function (req, res) {
+    var response;
+    var modelIpfsHash = req.body.modelIpfsHash;
+    var dataIpfsHash = req.body.dataIpfsHash;
+    var modelAddress = req.body.modelAddress;
+
+    if(modelIpfsHash === undefined || modelIpfsHash === ''||
+        dataIpfsHash===undefined|| dataIpfsHash===''||
+        modelAddress ===undefined|| modelAddress ==='') {
+        response = completeRes("参数不完全", 201);
+    }
+    else {
+
+        // todo 运算方调用容器后端
+
+    }
+    res.end(response);
+});
+
+router.post('/uploadencrypteddata', function (req, res) {
+    var response;
+    //todo
+    res.end(response);
+});
+
+
+router.post('/monitordata', function (req, res) {
+    var response;
+   // todo
+    res.end(response);
+});
+
+router.post('/monitormodel', function (req, res) {
+    var response;
+    // todo
+    res.end(response);
+});
+
+router.post('/getdockerstatus', function (req, res) {
+    var response;
+    // todo
+    res.end(response);
 });
 
 module.exports = router;
--- routes/modelclient.js
+++ routes/modelclient.js
@@ -10,8 +10,105 @@ function completeRes(msg, code){
 }
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Module client' });
+router.get('/index', function(req, res, next) {
+    res.sendFile( __dirname + "/pages/" + "indexmodel.html" );
+});
+router.get('/walletpage', function(req, res, next) {
+    res.sendFile( __dirname + "/pages/" + "modelclientwallet.html" );
+});
+router.get('/availabledatapage', function(req, res, next) {
+    res.sendFile( __dirname + "/pages/" + "model_available_data.html" );
+});
+
+
+router.post('/uploadfile', function (req, res) {
+    var response;
+    //todo
+    res.end(response);
+});
+
+router.post('/askdata', function (req, res) {
+    var response;
+    var password = req.body.password;
+    var from = req.body.from;
+    var metaDataInfo = req.body.metaDataInfo;
+
+    if(password === undefined || password === ''||
+        metaDataInfo===undefined|| metaDataInfo===''||
+        from ===undefined|| from ==='') {
+        response = completeRes("参数不完全", 201);
+    }
+    else {
+        //todo
+
+    }
+    res.end(response);
+});
+
+
+router.post('/createcontract', function (req, res) {
+    var response;
+    var password = req.body.password;
+
+    if(password === undefined || password === '') {
+        response = completeRes("参数不完全", 201);
+    }
+    else {
+        //todo
+
+    }
+    res.end(response);
+});
+
+router.post('/uploadmodel', function (req, res) {
+    var response;
+
+    var password = req.body.password;
+    var from = req.body.from;
+    var modelIpfsHash = req.body.modelIpfsHash;
+    var contractHash = req.body.contractHash;
+
+    if(password === undefined || password === ''||
+        modelIpfsHash===undefined|| modelIpfsHash===''||
+        contractHash===undefined|| contractHash===''||
+        from ===undefined|| from ==='') {
+        response = completeRes("参数不完全", 201);
+    }
+    else {
+        //todo
+
+    }
+    res.end(response);
+});
+
+
+router.post('/uploadresult', function (req, res) {
+    var response;
+    //todo
+    res.end(response);
+});
+
+
+router.post('/monitordataclient', function (req, res) {
+    var response;
+
+    //todo
+    res.end(response);
+});
+
+router.post('/monitorparameter', function (req, res) {
+    var response;
+    var modeladdress = req.body.modeladdress;
+
+    if(modeladdress === undefined || modeladdress === '') {
+        response = completeRes("参数不完全", 201);
+    }
+    else {
+        //todo
+
+    }
+
+    res.end(response);
 });
 
 module.exports = router;
